840e04f8565004dc5005dc2d53b67ad0468c602b
skhatri <servlet2@msn.com>
2024-01-10 15:13:27 +1100

    Add helidon starter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name="microservices-starter"
 
-listOf("spring-starter", "quarkus-starter", "pokemon-common").forEach { folder ->
+listOf("helidon-starter", "spring-starter", "quarkus-starter", "pokemon-common").forEach { folder ->
     include(folder)
     project(":${folder}").projectDir = file(folder)
 }
